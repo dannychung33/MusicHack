e67300451e4898869addc94748dbad8ff31d0175
Danny Chung <41158853+hachui@users.noreply.github.com>
2021-02-21 16:46:18 -0500

    key highlights working

--- src/index.js
+++ src/index.js
@@ -8,5 +8,19 @@ keys.forEach((key) => {
     key.addEventListener('mousedown', (e) => {
         console.log(e.target.dataset.note)
         synth.triggerAttackRelease(e.target.dataset.note, '8n');
+        e.target.style.backgroundColor = 'lightblue';
+        setTimeout(()=>{
+            changeBack(e.target);
+        }, 220);
+        console.log(e.target)
     });
 });
+
+function changeBack(target){
+    if(target.className === "black-key"){
+        target.style.backgroundColor = "black";
+    }
+    else{
+    target.style.backgroundColor = "white";
+    }
+}
