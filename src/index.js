5217caea5c15e980455b4ef4fd2478772b145e1d
unknown <jayshim0725@gmail.com>
2021-02-20 19:33:02 -0800

    new keyboard

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const synth = new Tone.Synth();
 synth.toMaster();
 
 const keys = document.querySelectorAll(".key");
+console.log(keys);
 
 keys.forEach((key) => {
-    key.addEventListener('mousedown', () => {
-        synth.triggerAttackRelease(key.id, '8n');
+    key.addEventListener('mousedown', (e) => {
+        console.log(e.target.dataset.note)
+        synth.triggerAttackRelease(e.target.dataset.note, '8n');
     });
 });
